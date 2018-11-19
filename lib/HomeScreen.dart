605911be5d4df13cb4b9593e93e042bfa9a287de
eduardolujan <infinito15A>
2018-11-18 18:38:06 -0600

    Initial commit

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+import 'package:flutter/foundation.dart';
+import 'Simon.dart';
+class HomeScreen extends StatefulWidget {
+  //final String userName;
+  final UserInfoDetails detailsUser;
+
+  HomeScreen({Key key, this.detailsUser}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("App Home Screen"),),
+      //body: Center(child: Text("Home Screen"),),
+      body: new Simon(),
+      drawer: Drawer(
+        child: ListView(children: <Widget>[
+          UserAccountsDrawerHeader(
+            accountName: Text(widget.detailsUser.displayName,),
+            accountEmail: Text(widget.detailsUser.email,),
+            currentAccountPicture: CircleAvatar(
+              // backgroundColor: Colors.black26,
+              child:  new Image.network(
+                widget.detailsUser.photoUrl,
+                colorBlendMode: BlendMode.dstIn,
+              ),
+            ),
+            //decoration: BoxDecoration(color: Colors.orange),
+          ),
+          AboutListTile(
+              child: Text("About"),
+              applicationName: "My Login App",
+              applicationVersion: "v1.0.0",
+              applicationIcon: Icon(Icons.adb),
+              icon: Icon(Icons.info)
+          ),
+          Divider(),
+          ListTile(
+            leading: new Icon(Icons.close),
+            title: new Text("Cerrar"),
+            onTap: () {
+              setState(() {
+                // pop closes the drawer
+                Navigator.pop(context);
+              });
+            },
+          )
+        ],),
+      ),
+    );
+  }
+}
---
+++ lib/Simon.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Simon extends StatefulWidget {
+  @override
+  _SimonState createState() => _SimonState();
+}
+
+class _SimonState extends State<Simon> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+
+    );
+  }
+}
